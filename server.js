963076ab907352b727416f29f1a4a1cc22d61468
lengwinc <lengwinc@oregonstate.edu>
2020-11-08 13:49:16 -0800

    first commit

---
+++ server.js
@@ -0,0 +1,71 @@
+var http = require('http');
+var fs = require('fs');
+var url = require('url');
+
+const m = new Map();
+
+function renderFile(path, res){
+	fs.readFile(path, function(error, data){
+		if(error){
+			res.writeHead(404);
+			res.wrtie('File not found');
+		}
+		else{
+			res.writeHead(200);
+
+			if(m.has(path)){
+				data = m.get(path);
+				res.write(data);
+			}
+			else{
+				res.write(data);
+				m.set(path, data);
+				console.log('data read');
+			}
+		}
+		res.end();
+	});
+}
+
+function requestHandler(req, res){
+//	console.log("<> Request aqquired");
+//	console.log("<><> req.method", req.method);
+//	console.log("<><> req.url:", req.url);
+//	console.log("<><> req.headers", req.headers);
+
+	var path = req.url;
+
+	switch(path){
+		case '/logs':
+			renderFile('./public/logs.html', res);
+      break;
+    case '/workouts':
+      renderFile('./public/workouts.html', res);
+			break;
+    case '/create':
+      renderFile('./public/create.html', res);
+      break;
+		case '/index.js':
+			renderFile('./public/index.js', res);
+			break;
+		case '/style.css':
+			renderFile('./public/style.css', res);
+			break;
+		case '/404.html':
+			renderFile('./public/404.html', res);
+			break;
+		case '/':
+			renderFile('./public/logs.html', res);
+			break;
+		default:
+			fs.readFile('./public/404.html', function(error, data){
+				res.writeHead(404);
+				res.write(data);
+				res.end();
+			});
+  }
+}
+
+var server = http.createServer(requestHandler);
+var port = process.env.PORT || 8611;
+server.listen(port);
